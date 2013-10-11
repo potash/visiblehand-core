a47047cb58b53e8334ddc8c1d2de7ff2074a5081
Eric Potash <eric@k2co3.net>
2013-10-11 18:02:22 -0500

    misplaced import

--- src/visiblehand/entity/Airline.java
+++ src/visiblehand/entity/Airline.java
@@ -1,21 +1,11 @@
 package visiblehand.entity;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-import visiblehand.data.CustomBooleanDeserializer;
 import lombok.Data;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.MappingIterator;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 
 @JsonPropertyOrder({ "id", "name", "alias", "iata", "icao", "callsign",
 		"country", "active" })
