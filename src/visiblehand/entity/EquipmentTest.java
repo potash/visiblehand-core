64788e2d13ca08d4f323ded2b2ddbb7c3be52dca
Eric Potash <eric@k2co3.net>
2013-10-21 16:43:33 -0500

    fuel data coverage tests

--- src/visiblehand/entity/EquipmentTest.java
+++ src/visiblehand/entity/EquipmentTest.java
@@ -1,57 +1,34 @@
 package visiblehand.entity;
 
-import static org.junit.Assert.*;
-
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
 
+import visiblehand.VisibleHandTest;
+
 import com.avaje.ebean.Ebean;
-import com.avaje.ebean.SqlQuery;
-import com.avaje.ebean.SqlRow;
 
-public class EquipmentTest {
+public class EquipmentTest extends VisibleHandTest {
 
+	// How many equipment have fuel data?
 	@Test
 	public void test() {
-		fail("This should test fueldata coverage of equipment");
-		String sql = "select e, count(*) c from (select unnest (equipment) e from route) t group by e order by count(*) desc";
-
-		// System.out.println(getId());
-		SqlQuery sqlQuery = Ebean.createSqlQuery(sql);
-
-		List<SqlRow> rows = sqlQuery.findList();
-		int count = 0, route_count = 0;
-		for (SqlRow row : rows) {
-			String iata = row.getString("e");
-			Equipment eq = Ebean.find(Equipment.class).where().eq("iata", iata)
-					.findUnique();
-
-			if (eq != null && eq.getFuelData().size() == 0) {
-				int aems = 0;
-				List<Equipment> relatives = eq.getParents();
-				relatives.addAll(eq.getChildren());
-				for (Equipment e : relatives) {
-					aems += e.getFuelData().size();
-				}
-				if (aems == 0
-						&& !(eq.getSimilar() != null && eq.getSimilar()
-								.getFuelData().size() > 0)) {
-					count++;
-					int c = row.getInteger("c");
-					route_count += c;
-					System.out.println(iata + ": " + c);
-					System.out.println(eq.getParents().size());
-					System.out.println(eq.getChildren().size());
-				}
+		List<Equipment> equipment = Ebean.find(Equipment.class).findList();
+		int count = 0; // the number of equipment with fuel data;
+		for (Equipment e : equipment) {
+			if (e.getAllFuelData().size() > 0) {
+				count++;
+			} else {
+				System.out.println(e);
 			}
 		}
-		System.out.println(count + " of " + rows.size() + " equipment codes = "
-				+ (double) count / rows.size() + "%");
-		int num_routes = Ebean.find(Route.class).findRowCount();
-		System.out.println(route_count + " of " + num_routes + " routes = "
-				+ (double) route_count / num_routes + "%");
-
+		System.out.println("Fuel data exists for " + count + " / "
+				+ equipment.size() + " = " + (double) count / equipment.size()*100
+				+ "% of equipment");
 	}
-
+	
+	@Test
+	public void test2() {
+	}
 }
--- src/visiblehand/entity/RouteTest.java
+++ src/visiblehand/entity/RouteTest.java
@@ -1,32 +1,45 @@
 package visiblehand.entity;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import visiblehand.VisibleHand;
+import visiblehand.VisibleHandTest;
+import visiblehand.parser.AirParser;
 
 import com.avaje.ebean.Ebean;
 
 
-public class RouteTest {
+public class RouteTest extends VisibleHandTest {
 
-	
-	@BeforeClass
-	public static void setUp() throws Exception {
-		VisibleHand.loadData();
-	}
-	
 	@Test
-	public void testGetEquipment() {
-		Route route = Ebean.find(Route.class, 4510);
-		assertEquals(route.getEquipment().size(), 4);
-	}
-	
-	@Test
-	public void testFuelBurn() {
-		assertTrue(Ebean.find(Route.class, 4510).getFuelBurn().getMean() != Double.NaN);
+	public void test() {
+		List<Airline> airlines = new ArrayList<Airline>();
+		for (AirParser parser : VisibleHand.airParsers) {
+			airlines.add(parser.getAirline());
+		}
+		List<Route> routes = Ebean.find(Route.class)
+				.where().in("airline", airlines)
+				.findList();
+		int count = 0; // the number of equipment with fuel data;
+		for (Route route : routes) {
+			boolean hasData = false;
+			for (Equipment e : route.getEquipment()) {
+				if (e.getAllFuelData().size() > 0) {
+					hasData = true; break;
+				}
+			}
+			if (hasData) {
+				count++;
+			} else {
+				System.out.println(route);
+			}
+		}
+		System.out.println("Fuel data exists for " + count + " / "
+				+ routes.size() + " = " + (double) count / routes.size()*100
+				+ "% of routes");
 	}
 
 }
