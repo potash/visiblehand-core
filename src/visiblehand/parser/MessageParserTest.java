aeed8c74ebe264454757d72aa0fce18c28de2609
Eric Potash <eric@k2co3.net>
2013-10-28 23:48:59 -0500

    Refactor parser testing code

--- src/visiblehand/parser/MessageParserTest.java
+++ src/visiblehand/parser/MessageParserTest.java
@@ -1,7 +1,15 @@
 package visiblehand.parser;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
 
 import visiblehand.VisibleHandTest;
 
@@ -11,10 +19,16 @@ public class MessageParserTest extends VisibleHandTest {
 			return !name.startsWith(".");
 		}
 	};
-
-	protected File[] getTestMessages() {
+	
+	protected Message[] getTestMessages(MessageParser parser) throws FileNotFoundException, MessagingException {
 		File dir = new File("data/mail/"
-				+ this.getClass().getSimpleName().replace("Test", ""));
-		return dir.listFiles(hiddenFileFilter);
+				+ parser.getClass().getSimpleName());
+		File[] files = dir.listFiles();
+		Message[] messages = new Message[files.length];
+		Session session = Session.getDefaultInstance(new Properties());
+		for (int i = 0; i < files.length; i++) {
+			messages[i] = new MimeMessage(session, new FileInputStream(files[i]));
+		}
+		return messages;
 	}
 }
